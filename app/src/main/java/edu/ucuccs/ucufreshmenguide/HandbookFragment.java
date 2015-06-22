522ce5bcf0622a461726fb89d04a279f4ec63055
ajdeguzman <wog.adeguzman@gmail.com>
2015-06-22 14:42:57 +0800

    inserted address in contact us

--- app/src/main/java/edu/ucuccs/ucufreshmenguide/HandbookFragment.java
+++ app/src/main/java/edu/ucuccs/ucufreshmenguide/HandbookFragment.java
@@ -13,7 +13,7 @@ public class HandbookFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.handbookfragment, container, false);
+        return inflater.inflate(R.layout.stud_org, container, false);
     }
 
 /*    @Override
